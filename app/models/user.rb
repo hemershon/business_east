c63f8b8dc03cb3929ec5a2a185060c48a6a85bdd
Hemershon Silva <hemershon@gmail.com>
2024-01-26 10:48:47 -0300

    feat devise registre

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  cnpj_cpf               :string
+#  company_name           :string
+#  corporate_name         :string
+#  description            :text
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  phone                  :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+  # validate :company_name, presence: true
+        end
